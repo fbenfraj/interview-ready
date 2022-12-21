9e9f8cdd68c64bb139bc605c2f12d6eaecf96159
Farouk Ben Fraj <farouk.ben-fraj@outlook.com>
2022-12-21 15:34:19 +0100

    add puppeteer scraping module (#3)

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OpenaiModule } from './openai/openai.module';
+import { ScrapeModule } from './scrape/scrape.module';
 
 @Module({
-  imports: [OpenaiModule],
+  imports: [ScrapeModule, OpenaiModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/scrape/scrape.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import * as puppeteer from 'puppeteer';
+import { HttpException, HttpStatus } from '@nestjs/common';
+
+@Injectable()
+export class ScrapeService {
+  async scrapeJobDescription(url: string): Promise<string> {
+    try {
+      const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
+      const page = await browser.newPage();
+
+      await page.goto(url);
+
+      const textContent = await page.evaluate(() => document.body.textContent);
+
+      await browser.close();
+
+      return textContent;
+    } catch (error) {
+      console.log(error);
+      throw new HttpException(
+        'Error scraping job description',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+}
