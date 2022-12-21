70641cf7db41324d737ab993f49f38a4b6b76fdf
fbenfraj <farouk.ben-fraj@outlook.com>
2022-12-21 15:46:50 +0100

    Fix: pdfParse using Uint8Array instead of Buffer

--- src/pdf-parse/pdf-parse.service.ts
+++ src/pdf-parse/pdf-parse.service.ts
@@ -7,13 +7,11 @@ export class PdfParseService {
   async parsePdf(filePath: string): Promise<string> {
     // Read the PDF file into a Buffer
     const buffer = fs.readFileSync(filePath);
-    // Convert the Buffer to a Uint8Array
-    const uint8Array = new Uint8Array(buffer);
-
     // Extract the text from the PDF using pdf-parse
-    const data = await pdfParse(uint8Array);
+    const data = await pdfParse(buffer);
     // Format the text to remove line breaks and trim whitespace
     const formatted = data.text.replace(/(\r\n|\n|\r)/gm, ' ').trim();
+    
     // Return the formatted text
     return formatted;
   }
